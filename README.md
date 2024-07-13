06cdc848f59cfd4ddf451f48b68d2edbc81e68a5
Fabian Ahammer <22956084+FabianAhammer@users.noreply.github.com>
2024-07-13 22:49:19 +0200

    create auto parsing on input files

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Python powered Eurocontrol FRA Point viewer
 
+Visualizes [Eurocontrol FRA Points](https://www.eurocontrol.int/publication/free-route-airspace-fra-points-list-ecac-area)
+
 This small visualizer just uses a wrapper library in python for leaflet,
 and shows FRA layers for each FRA Zone.
 
-Currently only uses the 2408 cycle, can be customized
-
-
+Currently only uses the 2408 cycle file, can be customized
 
 How to start:
 
@@ -16,3 +16,10 @@ How to start:
     ## start the parser
     python main.py
 ```
+
+
+How to create a custom FRA Point map:
+
+- Place a .xlsx FRA Point file inside the `input` folder
+- Start the Program
+- Open the generated *.html file for the FRA Points
--- main.py
+++ main.py
@@ -11,23 +11,25 @@ if not os.path.exists("./output"):
 # Define coordinates of where we want to center our map
 austria_coords = [47.517201, 14.238281]
 
-file = "eurocontrol-2408-fra-points-08aug2024.xlsx"
-data = pd.read_excel("input/%s" % file, sheet_name="FRA Points")
+available_files = filter(lambda name: name.__contains__(".xlsx"), os.listdir("./input"))
+for file in list(available_files):
+    print("Creating map for file: %s" % file)
+    data = pd.read_excel("input/%s" % file, sheet_name="FRA Points")
 
-base_map = folium.Map(location=austria_coords, zoom_start=6)
+    base_map = folium.Map(location=austria_coords, zoom_start=6)
 
-group = ""
-group_internal = None
-for it, datarow in data.iterrows():
-    if datarow.iloc[MarkerUtil.FRA_ZONE] != group:
-        group = datarow.iloc[MarkerUtil.FRA_ZONE]
-        group_internal = folium.FeatureGroup(group).add_to(base_map)
-    MarkerUtil.create_marker(group_internal, MarkerUtil.convert_lat(datarow.iloc[MarkerUtil.LAT]),
-                             MarkerUtil.convert_lon(datarow.iloc[MarkerUtil.LON]),
-                             MarkerUtil.parse_purpose(datarow.iloc[MarkerUtil.EXI], datarow.iloc[MarkerUtil.AD]),
-                             MarkerUtil.create_marker_header(datarow),
-                             MarkerUtil.create_text(datarow))
+    group = ""
+    group_internal = None
+    for it, datarow in data.iterrows():
+        if datarow.iloc[MarkerUtil.FRA_ZONE] != group:
+            group = datarow.iloc[MarkerUtil.FRA_ZONE]
+            group_internal = folium.FeatureGroup(group).add_to(base_map)
+        MarkerUtil.create_marker(group_internal, MarkerUtil.convert_lat(datarow.iloc[MarkerUtil.LAT]),
+                                 MarkerUtil.convert_lon(datarow.iloc[MarkerUtil.LON]),
+                                 MarkerUtil.parse_purpose(datarow.iloc[MarkerUtil.EXI], datarow.iloc[MarkerUtil.AD]),
+                                 MarkerUtil.create_marker_header(datarow),
+                                 MarkerUtil.create_text(datarow))
 
-folium.LayerControl().add_to(base_map)
+    folium.LayerControl().add_to(base_map)
 
-base_map.save("output/%s.html" % (file.split(".")[0]))
+    base_map.save("output/%s.html" % (file.split(".")[0]))
