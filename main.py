5c38da725c7ffb4bf0ff508f78d631212371571e
Fabian Ahammer <22956084+FabianAhammer@users.noreply.github.com>
2024-07-24 22:09:28 +0200

    Add Autodownload Code

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import os
+import requests
+import re
 
-import folium
-import pandas as pd
+from utlis.eurocontrol_file_downloader import EurocontrolFileDownloader
 
-from utlis.marker_util import MarkerUtil
+if __name__ == "__main__":
+    input_file_dir = "./input"
 
-if not os.path.exists("./output"):
-    os.mkdir("./output")
+    downloader = EurocontrolFileDownloader(input_file_dir)
+    downloader.check_and_download_missing_files()
 
-# Define coordinates of where we want to center our map
-austria_coords = [47.517201, 14.238281]
-
-available_files = filter(lambda name: name.__contains__(".xlsx"), os.listdir("./input"))
-for file in list(available_files):
-    print("Creating map for file: %s" % file)
-    data = pd.read_excel("input/%s" % file, sheet_name="FRA Points")
-
-    base_map = folium.Map(location=austria_coords, zoom_start=6)
-
-    group = ""
-    group_internal = None
-    for it, datarow in data.iterrows():
-        if datarow.iloc[MarkerUtil.FRA_ZONE] != group:
-            group = datarow.iloc[MarkerUtil.FRA_ZONE]
-            group_internal = folium.FeatureGroup(group).add_to(base_map)
-        MarkerUtil.create_marker(group_internal, MarkerUtil.convert_lat(datarow.iloc[MarkerUtil.LAT]),
-                                 MarkerUtil.convert_lon(datarow.iloc[MarkerUtil.LON]),
-                                 MarkerUtil.parse_purpose(datarow.iloc[MarkerUtil.EXI], datarow.iloc[MarkerUtil.AD]),
-                                 MarkerUtil.create_marker_header(datarow),
-                                 MarkerUtil.create_text(datarow))
-
-    folium.LayerControl().add_to(base_map)
-
-    base_map.save("output/%s.html" % (file.split(".")[0]))
+# if not os.path.exists("./output"):
+#     os.mkdir("./output")
+#
+# # Define coordinates of where we want to center our map
+# austria_coords = [47.517201, 14.238281]
+#
+# available_files = filter(lambda name: name.__contains__(".xlsx"), os.listdir("./input"))
+# for file in list(available_files):
+#     print("Creating map for file: %s" % file)
+#     data = pd.read_excel("input/%s" % file, sheet_name="FRA Points")
+#
+#     base_map = folium.Map(location=austria_coords, zoom_start=6)
+#
+#     group = ""
+#     group_internal = None
+#     for it, datarow in data.iterrows():
+#         if datarow.iloc[MarkerUtil.FRA_ZONE] != group:
+#             group = datarow.iloc[MarkerUtil.FRA_ZONE]
+#             group_internal = folium.FeatureGroup(group).add_to(base_map)
+#         MarkerUtil.create_marker(group_internal, MarkerUtil.convert_lat(datarow.iloc[MarkerUtil.LAT]),
+#                                  MarkerUtil.convert_lon(datarow.iloc[MarkerUtil.LON]),
+#                                  MarkerUtil.parse_purpose(datarow.iloc[MarkerUtil.EXI], datarow.iloc[MarkerUtil.AD]),
+#                                  MarkerUtil.create_marker_header(datarow),
+#                                  MarkerUtil.create_text(datarow))
+#
+#     folium.LayerControl().add_to(base_map)
+#
+#     base_map.save("output/%s.html" % (file.split(".")[0]))
---
+++ utlis/eurocontrol_file_downloader.py
@@ -0,0 +1,47 @@
+import requests
+import re
+import os
+
+
+class EurocontrolFileDownloader:
+
+    _base_uri: str = "https://www.eurocontrol.int"
+    _point_overview_uri: str = (
+        f"{_base_uri}/publication/free-route-airspace-fra-points-list-ecac-area"
+    )
+    _file_directory: str
+
+    def __init__(self, file_directory: str) -> None:
+        self._file_directory = file_directory
+
+    def check_and_download_missing_files(self) -> list[str]:
+        current_files = os.listdir(self._file_directory)
+        available_files = self.discover_files_to_download()
+
+        for available in available_files:
+            link = self.get_link_for_file(available)
+            file_name_for_available = link.split("/")[-1]
+            if not current_files.__contains__(file_name_for_available):
+                file = requests.get(link)
+                with open(
+                    f"{self._file_directory}/{file_name_for_available}", "wb"
+                ) as fh:
+                    fh.write(file.content)
+                    fh.flush()
+
+    def discover_files_to_download(self) -> list[str]:
+        site_content = str(requests.get(self._point_overview_uri).content)
+        available_docs = re.findall(
+            r'class="file__download">(.*?)</div>', site_content.replace("\n", "")
+        )
+        return available_docs
+
+    # 'https://www.eurocontrol.int/sites/default/files/2024-06/eurocontrol-2407-fra-points-11jul2024.xlsx'
+    def get_cycle_for_file_name(self, file_name: str) -> str:
+        return re.search("eurocontrol-(.*?)-", file_name).group(1)
+
+    def get_link_for_file(self, file_name: str) -> str:
+        return self._base_uri + re.search('href="(.*?)"', file_name).group(1)
+
+    def get_file_name_for_link(self, link: str) -> str:
+        return link.split("/")[-1]
